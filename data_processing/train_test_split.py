c7955ad16b02bc0db3d93659d9fc4b78dda3cde8
Santiago Tena Hernandez <stenahe@gmail.com>
2024-10-10 01:49:30 +0200

    refactor

--- data_processing/train_test_split.py
+++ data_processing/train_test_split.py
@@ -1,16 +1,15 @@
 import numpy as np
 
 def train_test_split(X, y, test_size=0.2, random_state=42):
-	sample_number = X.shape[0]
-	indices = np.arange(sample_number)
+	X_len = X.shape[0]
+	indices = np.arange(X_len)
 
 	np.random.seed(random_state)
 	np.random.shuffle(indices)
-
-	test_size = int(sample_number * test_size)
-
-	test_indices = indices[:test_size]
-	train_indices = indices[test_size:]
+	
+	test_elements = int(X_len * test_size)
+	test_indices = indices[:test_elements]
+	train_indices = indices[test_elements:]
 
 	X_train, X_test = X.iloc[train_indices], X.iloc[test_indices]
 	y_train, y_test = y.iloc[train_indices], y.iloc[test_indices]
